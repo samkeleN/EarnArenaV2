d4d02521a001e42f0fbe9f3b0042da830978505a
samkelen <samkelendzululeka@gmail.com>
2025-12-10 05:33:22 +0200

    chore: update dependencies in package.json
    - downgraded @react-navigation/native from ^7.1.25 to ^7.1.8
    - upgraded @tanstack/react-query from ^5.81.5 to ^5.90.12
    - updated expo and related packages to latest versions
    - upgraded react-native from 0.81.4 to 0.81.5
    - added react-native-worklets as a new dependency

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    plugins: [
+      // Other plugins first (e.g., for NativeWind, module-resolver)...
+      'react-native-worklets-core/plugin',  // Or 'react-native-worklets/plugin' if using the full package
+      'react-native-reanimated/plugin',     // Must be last
+    ],
   };
 };
