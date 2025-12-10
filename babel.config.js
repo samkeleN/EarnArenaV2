1627e7cdd7d94bd4020eefaaf9c09156922a10eb
samkelen <samkelendzululeka@gmail.com>
2025-12-10 05:34:30 +0200

    refactor: remove react-native-worklets-core plugin from babel configuration

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = function (api) {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
       // Other plugins first (e.g., for NativeWind, module-resolver)...
-      'react-native-worklets-core/plugin',  // Or 'react-native-worklets/plugin' if using the full package
       'react-native-reanimated/plugin',     // Must be last
     ],
   };
